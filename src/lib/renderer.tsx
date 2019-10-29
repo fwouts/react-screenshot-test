dd91a0bbd2808b35f080716c47d03eb6f6ada6a9
Francois Wouts <f@zenc.io>
2019-10-29 21:14:23 +1100

    Update renderer.tsx

--- src/lib/renderer.tsx
+++ src/lib/renderer.tsx
@@ -12,7 +12,7 @@ export class ScreenshotRenderer {
 
   async start() {
     this.browser = await puppeteer.launch({
-      args: ["--font-render-hinting=none"]
+      args: ["--font-render-hinting=medium"]
     });
     await this.server.start();
   }
