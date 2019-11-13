4bee8464b554fc1eeb05dc1fde451169fc5ac72f
Francois Wouts <f@zenc.io>
2019-11-14 10:04:54 +1100

    Set up example directory

---
+++ example/jest.screenshot.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: "node",
+  globalSetup: "react-screenshot-test/global-setup",
+  globalTeardown: "react-screenshot-test/global-teardown",
+  testMatch: ["**/?(*.)+(screenshot).[jt]s?(x)"]
+};
