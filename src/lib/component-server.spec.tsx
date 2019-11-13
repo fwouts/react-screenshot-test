37b0845059c3f58b31d602c05bdc5311a63b7edb
Francois Wouts <f@zenc.io>
2019-11-14 09:25:19 +1100

    Addd a test for ReactComponentServer

---
+++ src/lib/component-server.spec.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import React from "react";
+import { ReactComponentServer } from "./component-server";
+
+describe("ReactComponentServer", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("renders the requested node", async () => {
+    const server = new ReactComponentServer();
+    await server.start();
+    let rendered = false;
+    await server.serve(<div>Hello, World!</div>, async (port, path) => {
+      const { data } = await axios.get(`http://localhost:${port}${path}`);
+      // Fuzzy match.
+      expect(data).toContain("<div>Hello, World!</div>");
+      // Exact match.
+      expect(data).toMatchInlineSnapshot(
+        `"<html data-reactroot=\\"\\"><head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1.0\\"/></head><body><div>Hello, World!</div></body></html>"`
+      );
+      rendered = true;
+    });
+    expect(rendered).toBe(true);
+    await server.stop();
+  });
+});
