238df73c4eb4364faf4e5226bd1d9904c19b5b71
Francois Wouts <f@zenc.io>
2019-11-14 10:13:24 +1100

    Ignore example dir from root dir

--- jest.screenshot.config.js
+++ jest.screenshot.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testEnvironment: "node",
   globalSetup: "./src/lib/global-setup.ts",
   globalTeardown: "./src/lib/global-teardown.ts",
-  testMatch: ["**/?(*.)+(screenshot).[jt]s?(x)"]
+  testMatch: ["**/?(*.)+(screenshot).[jt]s?(x)"],
+  testPathIgnorePatterns: ["/dist/", "/example/", "/node_modules/"]
 };
