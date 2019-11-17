ab0d08ed5769705da64e820b57add824b47a430f
Francois Wouts <f@zenc.io>
2019-11-17 13:29:17 +1100

    Add back getCacheKey(), as the default is too aggressive

--- css-transform/index.js
+++ css-transform/index.js
@@ -1,6 +1,11 @@
 // Note: this was forked from
 // https://github.com/dferber90/jest-transform-css/blob/master/index.js
 
+// Note: you must increment this version whenever you update this script or
+// anything that it uses.
+const TRANSFORM_VERSION = "1";
+
+const crypto = require("crypto");
 const { cosmiconfigSync } = require("cosmiconfig");
 const crossSpawn = require("cross-spawn");
 
@@ -8,6 +13,29 @@ const explorer = cosmiconfigSync("react-screenshot-test");
 const transformConfig = explorer.search();
 
 module.exports = {
+  getCacheKey: (fileData, filename, configString, { instrument }) => {
+    return (
+      crypto
+        .createHash("md5")
+        .update(TRANSFORM_VERSION)
+        .update("\0", "utf8")
+        .update(fileData)
+        .update("\0", "utf8")
+        .update(filename)
+        .update("\0", "utf8")
+        .update(configString)
+        .update("\0", "utf8")
+        .update(JSON.stringify(transformConfig))
+        // TODO load postcssrc (the config) sync and make it part of the cache
+        // key
+        // .update("\0", "utf8")
+        // .update(getPostCssConfig(filename))
+        .update("\0", "utf8")
+        .update(instrument ? "instrument" : "")
+        .digest("hex")
+    );
+  },
+
   process: (src, filename) => {
     // No modules processing when plain CSS is used.
     // You can create react-screenshot-test.config.js in your project and add
