cd7e4f20537a412b35a33e35b5f66a1b653ae486
François Wouts <f@zenc.io>
2020-06-11 18:37:14 +1000

    Fix linting error

--- src/tests/components/static-image.tsx
+++ src/tests/components/static-image.tsx
@@ -5,4 +5,4 @@ const Image = styled.img`
   width: 100%;
 `;
 
-export const StaticImageComponent = () => <Image src={"/public/react.png"} />;
+export const StaticImageComponent = () => <Image src="/public/react.png" />;
