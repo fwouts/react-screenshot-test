e5a126d5c5496c7e91fcf9480e0bbe32a8fc6340
François Wouts <f@zenc.io>
2020-06-11 18:45:37 +1000

    Fix issue with public directory being git ignored

--- src/tests/images.screenshot.tsx
+++ src/tests/images.screenshot.tsx
@@ -1,4 +1,3 @@
-import { resolve } from "path";
 import React from "react";
 import { ReactScreenshotTest } from "../lib";
 import { PngComponent } from "./components/png";
@@ -8,7 +7,7 @@ import { VIEWPORTS } from "./viewports";
 
 ReactScreenshotTest.create("Images")
   .viewports(VIEWPORTS)
-  .static("/public", resolve("src/tests/public"))
+  .static("/public", "src/tests/public")
   .shoot("PNG", <PngComponent />)
   .shoot("SVG", <SvgComponent />)
   .shoot("Static image", <StaticImageComponent />)
